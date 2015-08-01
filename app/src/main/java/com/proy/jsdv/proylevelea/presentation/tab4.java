0c96751a8e6a21c2726fc98737bb8a3997edb529
Vico09 <migue.m09@gmail.com>
2015-08-01 16:38:42 -0600

    Problema Nullable solucionado
    importa paquete para solucionar problema de Nullable

--- app/src/main/java/com/proy/jsdv/proylevelea/presentation/tab4.java
+++ app/src/main/java/com/proy/jsdv/proylevelea/presentation/tab4.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.proy.jsdv.proylevelea.R;
 import com.proy.jsdv.proylevelea.menu.MainActivity;
